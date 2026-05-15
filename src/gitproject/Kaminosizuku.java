9236ed4c32139878f375f876690ed3649f554415
student <student@FJ-1025>
2026-05-15 12:12:38 +0900

    Javaファイル登録

---
+++ src/gitproject/Kaminosizuku.java
@@ -0,0 +1,8 @@
+package gitproject;
+
+public class Kaminosizuku {
+	public static void main(String[]args) {
+		System.out.print("uadsadsad");
+	}
+
+}
---
+++ src/gitproject/kamisininoyari.java
@@ -0,0 +1,10 @@
+package gitproject;
+
+public class kamisininoyari {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
